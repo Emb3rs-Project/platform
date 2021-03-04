b8ca4a20cb3030648142b39217ce8e80b0d6496f
dfernandes <davidsf@pantherify.com>
2021-03-04 15:58:37 +0100

    badumTSS

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,37 +1,50 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-        <div class="py-12 h-full">
-            <div class="h-full max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="h-full grid grid-cols-2 grid-rows-3 gap-x-6 grid-flow-row">
-                    <div class="h-full row-span-3 bg-gray-300 shadow-lg">
-                        Map
-                    </div>
-                    <div>
-                        <div class="bg-gray-300 h-4/5 rounded mb-3">Last News</div>
-                        <div class="bg-gray-300 h-4/5 rounded mb-3">Last Notifications</div>
-                        <div class="bg-gray-300 h-4/5 rounded">Lorem</div>
-                    </div>
-                </div>
-            </div>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Dashboard
+      </h2>
+    </template>
+
+    <div class="flex p-5">
+      <div class="w-8/12">
+        <leaflet-map></leaflet-map>
+      </div>
+      <div class="w-4/12 text-gray-700">
+        <div class="h-56 mb-5 p-3 border border-gray-200 shadow">
+          <h1 class="leading-5 font-bold mb-3">Latest news</h1>
+          <span
+            >Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Temporibus fugit non repellat iure ex alias delectus similique.
+            Similique ab obcaecati, ut accusamus quam, cupiditate placeat minima
+            neque eum delectus itaque.</span
+          >
         </div>
-        <div class="container mx-auto"></div>
-    </app-layout>
+        <div class="h-56 my-5 p-3 border border-gray-200 shadow">
+          <h1 class="leading-5 font-bold mb-3">Latest notifications</h1>
+          <span
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            inventore incidunt harum autem consectetur commodi, doloremque error
+            corrupti, doloribus cum quasi. Culpa provident quis facilis est.
+            Aspernatur explicabo sapiente velit?</span
+          >
+        </div>
+      </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
-import Welcome from "@/Jetstream/Welcome";
+import JetLabel from "@/Jetstream/Label";
+import LeafletMap from "@/Components/LeafletMap";
 
 export default {
-    components: {
-        AppLayout,
-        Welcome,
-    },
+  components: {
+    AppLayout,
+    JetLabel,
+    LeafletMap,
+  },
 };
 </script>
 
--- resources/js/Pages/Institution/InstitutionIndex.vue
+++ resources/js/Pages/Institution/InstitutionIndex.vue
@@ -1,88 +1,131 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Institution
-            </h2>
-        </template>
-        <div class="py-12 h-full">
-            <div class="h-full max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="h-full grid grid-cols-2 grid-rows-2 gap-x-6 grid-flow-row">
-                    <div class="h-full row-span-2 bg-gray-300 shadow-lg">
-                        Map
-                    </div>
-                    <div>
-                        <div class="flex flex-col bg-gray-300 h-4/5 rounded mb-3">
-                            <h2 class="p-5 font-semibold text-2xl text-gray-800 leading-tight">
-                                Users
-                            </h2>
-                            <div class="flex flex-col justify-center">
-                                <div
-                                    class="w-full bg-white rounded mb-3 text-xl"
-                                    v-for="user in users"
-                                    :key="user.name"
-                                >
-                                    {{user.id}} - {{ user.name }} - {{ user.username}}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="bg-gray-300 h-4/5 rounded mb-3">
-                            <h2 class="p-5 font-semibold text-md text-gray-800 leading-tight">
-                                Teams
-                            </h2>
-                            <div class="ml-5">
-                                <ul>
-                                    <li
-                                        v-for="team in teams"
-                                        :key="team.name"
-                                    >
-                                        {{ team.name }}
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Institution
+      </h2>
+    </template>
+    <div class="flex p-5">
+      <div class="w-8/12">
+        <leaflet-map></leaflet-map>
+      </div>
+      <div class="w-4/12 text-gray-700">
+        <div
+          class="h-80 mb-5 p-3 overflow-y-scroll border border-gray-200 shadow"
+        >
+          <h1 class="leading-5 font-bold mb-3">Users</h1>
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Email
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="user in users" :key="user.id">
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ user.name }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ user.username }}@somewhere.com
+                </td>
+              </tr>
+
+              <!-- More items... -->
+            </tbody>
+          </table>
         </div>
-        <div class="container mx-auto"></div>
-    </app-layout>
+        <div
+          class="h-80 my-5 p-3 overflow-y-scroll border border-gray-200 shadow"
+        >
+          <h1 class="leading-5 font-bold mb-3">Accounts</h1>
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Email
+                </th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="user in users" :key="user.id">
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ user.name }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ user.username }}@somewhere.com
+                </td>
+              </tr>
+
+              <!-- More items... -->
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="container mx-auto"></div>
+  </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
+import LeafletMap from "@/Components/LeafletMap";
 
 export default {
-    components: {
-        AppLayout,
-    },
-    data() {
-        return {
-            users: [{
-                id: '1',
-                name: 'George Mantellos',
-                username: 'geocfu'
-            }, {
-                id: '2',
-                name: 'David Fernandes',
-                username: 'davidfs'
-            },
-            {
-                id: '3',
-                name: 'Luis Miguel Campos',
-                username: 'luismc'
-            },
-            {
-                id: '4',
-                name: 'Carlos Nuno Marques',
-                username: 'carlosnm'
-            }],
-            teams: [{
-                name: 'George\'s team'
-            }]
-        }
-    }
-
+  components: {
+    AppLayout,
+    LeafletMap,
+  },
+  data() {
+    return {
+      users: [
+        {
+          id: "1",
+          name: "George Mantellos",
+          username: "geocfu",
+        },
+        {
+          id: "2",
+          name: "David Fernandes",
+          username: "davidfs",
+        },
+        {
+          id: "3",
+          name: "Luis Miguel Campos",
+          username: "luismc",
+        },
+        {
+          id: "4",
+          name: "Carlos Nuno Marques",
+          username: "carlosnm",
+        },
+      ],
+      teams: [
+        {
+          name: "George's team",
+        },
+      ],
+    };
+  },
 };
 </script>
 
