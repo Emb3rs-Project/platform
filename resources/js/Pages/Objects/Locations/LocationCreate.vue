f9db7a1838c3fe4cae335c7cf6cfe4d6112948a3
dfernandes <davidsf@pantherify.com>
2021-03-04 13:22:19 +0100

    new LeafletMap

--- resources/js/Pages/Objects/Locations/LocationCreate.vue
+++ resources/js/Pages/Objects/Locations/LocationCreate.vue
@@ -1,21 +1,26 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Objects | Locations
-            </h2>
-        </template>
-        <h1>Create new Location</h1>
-    </app-layout>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Objects | Locations
+      </h2>
+    </template>
+    <div class="grid grid-col-2">
+      <div>
+        <h2>New Location</h2>
+      </div>
+      <div></div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
 
 export default {
-    components: {
-        AppLayout,
-    },
+  components: {
+    AppLayout,
+  },
 };
 </script>
 
