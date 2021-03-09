64fc6a4b554d8d7cc53ba3b22c962798b93a8ee7
George Mantellos <gmantellos@pdmfc.com>
2021-03-09 18:02:55 +0100

    :bug: Fixed map and text appearence

--- resources/js/Pages/Objects/Locations/LocationIndex.vue
+++ resources/js/Pages/Objects/Locations/LocationIndex.vue
@@ -1,26 +1,30 @@
 <template>
-  <app-layout>
-    <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Objects | Locations
-      </h2>
-    </template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Objects | Locations
+            </h2>
+        </template>
 
-    <div class="w-full my-5 px-10 flex justify-end">
-      <input
-        class="border rounded-full bg-gray-200 border-gray-200 outline-none pl-5 leading-3"
-        placeholder="search..."
-      />
-    </div>
+        <div class="w-full my-5 px-10 flex justify-end">
+            <input
+                type="text"
+                class="border rounded-full bg-gray-200 border-gray-200 outline-none pl-5 leading-3"
+                placeholder="search..."
+            />
+        </div>
+        <div class="flex p-5 h-screen md:h-content">
+            <div class=" w-full">
+                <leaflet-map></leaflet-map>
+            </div>
+        </div>
 
-    <leaflet-map></leaflet-map>
-
-    <div class="w-full my-5 px-10 flex justify-end">
-      <jet-link-button path="objects.locations.create">
-        Create New Location
-      </jet-link-button>
-    </div>
-  </app-layout>
+        <div class="w-full my-5 px-10 flex justify-end">
+            <jet-link-button path="objects.locations.create">
+                Create New Location
+            </jet-link-button>
+        </div>
+    </app-layout>
 </template>
 
 <script>
@@ -31,20 +35,20 @@ import JetLabel from "@/Jetstream/Label";
 import LeafletMap from "@/Components/LeafletMap";
 
 export default {
-  components: {
-    AppLayout,
-    JetInput,
-    JetLabel,
-    JetLinkButton,
-    LeafletMap,
-  },
-  data() {
-    return {
-      search: "",
-    };
-  },
+    components: {
+        AppLayout,
+        JetInput,
+        JetLabel,
+        JetLinkButton,
+        LeafletMap,
+    },
+    data() {
+        return {
+            search: "",
+        };
+    },
 
-  methods: {},
+    methods: {},
 };
 </script>
 
