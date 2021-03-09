2bee2ef2d99fbf56f2d254fb30c075550f2d1570
George Mantellos <gmantellos@pdmfc.com>
2021-03-09 16:55:37 +0100

    :sparkles: SourceCreate page is ready for the map

--- resources/js/Pages/Objects/Sources/SourceCreate.vue
+++ resources/js/Pages/Objects/Sources/SourceCreate.vue
@@ -8,73 +8,98 @@
         <div class="p-5">
             <h1 class="text-lg font-bold">New Source</h1>
         </div>
-        <div class="flex p-5 md:h-auto">
-            <div class="w-6/12 h-3/4 md:overflow-y-auto md:pr-4">
-                <input-row
-                    heading="Details | Source"
-                    desc="Enter a name for your source"
-                >
-                    Source Name
-                </input-row>
+        <form @submit.prevent="submit">
+            <div class="flex p-5 h-content gap-2">
+                <div class="w-6/12 md:overflow-y-auto md:pr-4">
+                    <input-row
+                        heading="Details | Source"
+                        desc="Enter a name for your Source"
+                        v-model="form.sourceName"
+                    >
+                        Source Name
+                    </input-row>
+                    <jet-input-error
+                        :message="form.errors.sourceName"
+                        class="mt-2"
+                    />
 
-                <select-row
-                    desc="Choose how you want to define your Source. Dont worry if you cant find a prefect match. Choose the one that best describes your source."
-                    :options="typeOptions"
-                    class="mt-5"
-                >
-                    The type of source?
-                </select-row>
+                    <select-row
+                        class="mt-5"
+                        desc="Choose how you want to define your Source. Dont worry if you cant find a prefect match. Choose the one that best describes your source."
+                        :options="sourceTypes"
+                        v-model="form.sourceType"
+                    >
+                        Source Type
+                    </select-row>
 
-                <select-row
-                    desc="Try to identify the size of the source"
-                    :options="sizeOptions"
-                    class="mt-5"
-                >
-                    The size of the source?
-                </select-row>
+                    <select-row
+                        class="mt-5"
+                        desc="Try to idetify the size of the Source"
+                        :options="sourceSizes"
+                        v-model="form.sourceSize"
+                    >
+                        Source Size
+                    </select-row>
 
-                <select-row
-                    desc="Try to identify the fluid of the source"
-                    :options="fluidOptions"
-                    class="mt-5"
-                >
-                    The fluid of the source?
-                </select-row>
+                    <select-row
+                        class="mt-5"
+                        desc="Try to idetify the fluid of the Source"
+                        :options="sourceFluids"
+                        v-model="form.sourceFluid"
+                    >
+                        Source Fluid Type
+                    </select-row>
 
-                <select-row
-                    desc="Try to identify the temperature of the source"
-                    :options="temperatureOptions"
-                    class="mt-5"
-                >
-                    The temperature of the source?
-                </select-row>
+                    <select-row
+                        class="mt-5"
+                        desc="Try to idetify the temperature of the Source"
+                        :options="sourceTemperatures"
+                        v-model="form.sourceTemperature"
+                    >
+                        Source Temperature
+                    </select-row>
 
-                <radio-row
-                    desc="Select how you want to share your Location"
-                    :options="radioOptions"
-                    class="mt-5"
-                >
-                    Location sharing options
-                </radio-row>
+                    <select-row
+                        class="mt-5"
+                        desc="Try to idetify the presure of the Source"
+                        :options="sourcePressures"
+                        v-model="form.sourcePressure"
+                    >
+                        Source Presure
+                    </select-row>
+
+                    <select-row
+                        class="mt-14"
+                        heading="Details | Source + Location"
+                        desc="Associate this Source with a Location"
+                        :options="sourceLocations"
+                        v-model="form.sourceLocation"
+                    >
+                        Location Name
+                    </select-row>
+                </div>
+
+                <div class=" w-6/12">
+                    <leaflet-map></leaflet-map>
+                </div>
             </div>
-
-            <div class="w-6/12">
-                <leaflet-map></leaflet-map>
+            <div class="w-full my-5 px-16 flex justify-end">
+                <jet-button :disabled="form.processing">Create Location</jet-button>
             </div>
-        </div>
-        <div class="w-full my-5 px-16 flex justify-end">
-            <jet-link-button path="objects.sources.create"> Create </jet-link-button>
-        </div>
+        </form>
     </app-layout>
 </template>
 
 <script>
+import { useForm } from '@inertiajs/inertia-vue3'
+
 import AppLayout from "@/Layouts/AppLayout";
 import LeafletMap from "@/Components/LeafletMap";
 import InputRow from "@/Components/InputRow";
 import RadioRow from "@/Components/RadioRow";
 import SelectRow from "@/Components/SelectRow";
-import JetLinkButton from "@/Jetstream/LinkButton";
+import JetButton from "@/Jetstream/Button";
+import JetInputError from "@/Jetstream/InputError";
 
 export default {
     components: {
@@ -83,35 +108,67 @@ export default {
         InputRow,
         RadioRow,
         SelectRow,
-        JetLinkButton,
+        JetButton,
+        JetInputError
     },
-    data() {
+
+    setup() {
+        const form = useForm({
+            sourceName: '',
+            sourceType: 'Metallurgy',
+            sourceSize: 'Small',
+            sourceFluid: 'Water',
+            sourceTemperature: 'Cold',
+            sourcePressure: 'Low',
+            sourceLocation: 'Greece',
+        });
+
+        const sourceTypes = [
+            'Metallurgy',
+            'Timber',
+        ];
+        const sourceSizes = [
+            'Small',
+            'Medium',
+            'Normal',
+            'Big',
+            'Very Big',
+        ];
+        const sourceFluids = [
+            'Water',
+            'Steam',
+            'Petrolium',
+        ];
+        const sourceTemperatures = [
+            'Cold',
+            'Little Hot',
+            'Very Hot',
+        ];
+        const sourcePressures = [
+            'Low',
+            'Normal',
+            'High',
+        ];
+        const sourceLocations = [
+            'Greece',
+            'Portugal',
+        ];
+
+        const submit = () => {
+            console.log('submitting');
+            // TODO: Uncomment when backend is ready to accept data
+            // form.value.post(route('objects.locations.create'));
+        }
+
         return {
-            typeOptions: [
-                'Metallurgy',
-                'Timber',
-            ],
-            sizeOptions: [
-                'Small',
-                'Medium',
-                'Normal',
-                'Big',
-                'Very Big',
-            ],
-            fluidOptions: [
-                'Water',
-                'Steam',
-                'Petrolium',
-            ],
-            temperatureOptions: [
-                'Cold',
-                'Little Hot',
-                'Very Hot',
-            ],
-            radioOptions: [
-                'I want to share my Location with my Institution',
-                'I want to share my Location with everyone'
-            ]
+            form,
+            sourceTypes,
+            sourceSizes,
+            sourceFluids,
+            sourceTemperatures,
+            sourcePressures,
+            sourceLocations,
+            submit
         }
     }
 };
