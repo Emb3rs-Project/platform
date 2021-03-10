bf0a5f010839eef154866e071e9c199b33cd08ac
George Mantellos <gmantellos@pdmfc.com>
2021-03-10 15:20:51 +0100

    :sparkles: Added inverse relation on Template

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Template extends Model
 {
     use HasFactory, SoftDeletes;
+
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
