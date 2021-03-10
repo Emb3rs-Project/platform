e949e572bdba66314957f75682f4f45e924efa6b
dfernandes <davidsf@pantherify.com>
2021-03-10 15:35:42 +0100

    updated unitconv

--- app/Models/UnitConversion.php
+++ app/Models/UnitConversion.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UnitConversion extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function from() : BelongsTo
+    {
+        return $this->belongsTo(Unit::class, 'from_id');
+    }
+
+    public function to(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class, 'to_id');
+    }
 }
