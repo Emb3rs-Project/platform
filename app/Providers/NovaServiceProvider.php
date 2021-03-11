5abef5a9876e6bec2fbc6f1852a6e2baae77d131
dfernandes <davidsf@pantherify.com>
2021-03-11 19:17:57 +0100

    updating gitignore

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,7 +43,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'davidsf@pantherify.com',
+                'carlos.marques@pdmfc.com',
+                'gmantellos@pdmfc.com'
             ]);
         });
     }
