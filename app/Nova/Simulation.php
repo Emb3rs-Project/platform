dc4e65dda5aea34b12e7e042989c6012612629f6
dfernandes <davidsf@pantherify.com>
2021-03-11 11:37:30 +0100

    generated Models

---
+++ app/Nova/Simulation.php
@@ -0,0 +1,104 @@
+<?php
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\KeyValue;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Simulation extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var  string
+     */
+    public static $model = \App\Models\Simulation::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var  string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var  array
+     */
+    public static $search = [
+        'id',
+
+
+// You can add any of this to your Laravel Nova Search
+//    'status',
+//    'targetData',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+            Text::make(__('STATUS'), 'status'),
+            KeyValue::make(__('TARGETDATA'), 'targetData'),
+
+            BelongsTo::make(__('PROJECT'), 'project', Project::class),
+            BelongsTo::make(__('TARGET'), 'target', Target::class),
+            BelongsTo::make(__('SIMULATIONTYPE'), 'simulationType', SimulationType::class),
+
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param    \Illuminate\Http\Request  $request
+     * @return  array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
