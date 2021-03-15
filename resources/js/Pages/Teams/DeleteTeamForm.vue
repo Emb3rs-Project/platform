9519c071d98138aad460c266aaa52df79029aab0
dfernandes <davidsf@pantherify.com>
2021-03-15 13:55:38 +0100

    updating

--- resources/js/Pages/Teams/DeleteTeamForm.vue
+++ resources/js/Pages/Teams/DeleteTeamForm.vue
@@ -1,83 +1,89 @@
 <template>
-    <jet-action-section>
-        <template #title>
-            Delete Team
-        </template>
+  <jet-action-section>
+    <template #title> Delete Institution </template>
 
-        <template #description>
-            Permanently delete this team.
-        </template>
+    <template #description> Permanently delete this Institution. </template>
+
+    <template #content>
+      <div class="max-w-xl text-sm text-gray-600">
+        Once a Institution is deleted, all of its resources and data will be
+        permanently deleted. Before deleting this institution, please download
+        any data or information regarding this institution that you wish to
+        retain.
+      </div>
+
+      <div class="mt-5">
+        <jet-danger-button @click="confirmTeamDeletion">
+          Delete Institution
+        </jet-danger-button>
+      </div>
+
+      <!-- Delete Team Confirmation Modal -->
+      <jet-confirmation-modal
+        :show="confirmingTeamDeletion"
+        @close="confirmingTeamDeletion = false"
+      >
+        <template #title> Delete Institution </template>
 
         <template #content>
-            <div class="max-w-xl text-sm text-gray-600">
-                Once a team is deleted, all of its resources and data will be permanently deleted. Before deleting this team, please download any data or information regarding this team that you wish to retain.
-            </div>
-
-            <div class="mt-5">
-                <jet-danger-button @click="confirmTeamDeletion">
-                    Delete Team
-                </jet-danger-button>
-            </div>
-
-            <!-- Delete Team Confirmation Modal -->
-            <jet-confirmation-modal :show="confirmingTeamDeletion" @close="confirmingTeamDeletion = false">
-                <template #title>
-                    Delete Team
-                </template>
-
-                <template #content>
-                    Are you sure you want to delete this team? Once a team is deleted, all of its resources and data will be permanently deleted.
-                </template>
-
-                <template #footer>
-                    <jet-secondary-button @click="confirmingTeamDeletion = false">
-                        Cancel
-                    </jet-secondary-button>
-
-                    <jet-danger-button class="ml-2" @click="deleteTeam" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                        Delete Team
-                    </jet-danger-button>
-                </template>
-            </jet-confirmation-modal>
+          Are you sure you want to delete this Institution? Once an institution
+          is deleted, all of its resources and data will be permanently deleted.
         </template>
-    </jet-action-section>
+
+        <template #footer>
+          <jet-secondary-button @click="confirmingTeamDeletion = false">
+            Cancel
+          </jet-secondary-button>
+
+          <jet-danger-button
+            class="ml-2"
+            @click="deleteTeam"
+            :class="{ 'opacity-25': form.processing }"
+            :disabled="form.processing"
+          >
+            Delete Institution
+          </jet-danger-button>
+        </template>
+      </jet-confirmation-modal>
+    </template>
+  </jet-action-section>
 </template>
 
 <script>
-    import JetActionSection from '@/Jetstream/ActionSection'
-    import JetConfirmationModal from '@/Jetstream/ConfirmationModal'
-    import JetDangerButton from '@/Jetstream/DangerButton'
-    import JetSecondaryButton from '@/Jetstream/SecondaryButton'
+import JetActionSection from "@/Jetstream/ActionSection";
+import JetConfirmationModal from "@/Jetstream/ConfirmationModal";
+import JetDangerButton from "@/Jetstream/DangerButton";
+import JetSecondaryButton from "@/Jetstream/SecondaryButton";
 
-    export default {
-        props: ['team'],
+export default {
+  props: ["team"],
 
-        components: {
-            JetActionSection,
-            JetConfirmationModal,
-            JetDangerButton,
-            JetSecondaryButton,
-        },
+  components: {
+    JetActionSection,
+    JetConfirmationModal,
+    JetDangerButton,
+    JetSecondaryButton,
+  },
 
-        data() {
-            return {
-                confirmingTeamDeletion: false,
-                deleting: false,
+  data() {
+    return {
+      confirmingTeamDeletion: false,
+      deleting: false,
 
-                form: this.$inertia.form()
-            }
-        },
+      form: this.$inertia.form(),
+    };
+  },
 
-        methods: {
-            confirmTeamDeletion() {
-                this.confirmingTeamDeletion = true
-            },
+  methods: {
+    confirmTeamDeletion() {
+      this.confirmingTeamDeletion = true;
+    },
 
-            deleteTeam() {
-                this.form.delete(route('teams.destroy', this.team), {
-                    errorBag: 'deleteTeam'
-                });
-            },
-        },
-    }
+    deleteTeam() {
+      this.form.delete(route("teams.destroy", this.team), {
+        errorBag: "deleteTeam",
+      });
+    },
+  },
+};
 </script>
