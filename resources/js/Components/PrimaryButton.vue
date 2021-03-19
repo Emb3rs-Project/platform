e4b30f3f3bfe2f29d7b1a78c14b9a6cd37d0975c
George Mantellos <gmantellos@pdmfc.com>
2021-03-19 23:22:24 +0100

    :art: Global UI changes

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,7 +1,7 @@
 <template>
     <button
         type="button"
-        class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white uppercase bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
+        class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
     >
         <slot></slot>
     </button>
