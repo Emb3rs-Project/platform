ac169f14f1d92bc23e810dba5632ae02ebc1cbc2
George Mantellos <gmantellos@pdmfc.com>
2021-04-01 14:25:05 +0200

    :sparkles: Checkboxes on Sources index table

--- resources/js/Pages/Objects/Sources/SourceIndex.vue
+++ resources/js/Pages/Objects/Sources/SourceIndex.vue
@@ -6,9 +6,7 @@
       </h2>
     </template>
 
-    <div
-      class="flex flex-col md:flex-row h-screen md:h-table-and-map p-5 gap-5"
-    >
+    <div class="flex flex-col md:flex-row h-screen md:h-table-and-map p-5 gap-5">
       <div class="w-full h-96 md:h-full md:w-1/2">
         <div class="flex flex-col max-h-full">
           <div class="px-4 py-5 sm:px-6 bg-white shadow sm:rounded-t-md">
@@ -20,13 +18,16 @@
               <table class="min-w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                   <tr>
-                    <th scope="col" class="relative px-6 py-3">
+                    <th
+                      scope="col"
+                      class="relative px-6 py-3"
+                    >
                       <div class="flex justify-center items-center h-5">
                         <input
-                          name="comments"
                           type="checkbox"
                           class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                          v-model="massAction"
+                          v-model="massSelection"
+                          :indeterminate="massSelectionIndeterminated"
                         />
                       </div>
                     </th>
@@ -60,7 +61,10 @@
                     >
                       Location
                     </th>
-                    <th scope="col" class="relative px-6 py-3">
+                    <th
+                      scope="col"
+                      class="relative px-6 py-3"
+                    >
                       <span class="sr-only">Edit</span>
                     </th>
                   </tr>
@@ -73,42 +77,35 @@
                   >
                     <td class="px-6 py-4 whitespace-nowrap">
                       <div class="flex justify-center items-center h-5">
-                        <!-- <input
+                        <input
                           :name="source.name"
                           :value="source"
                           v-model="selected"
                           type="checkbox"
                           class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded disabled:opacity-50"
                           :disabled="source.location ? false : true"
-                        /> -->
+                        />
                         <!-- This example requires Tailwind CSS v2.0+ -->
                         <!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
-                        <tris-checkbox></tris-checkbox>
+                        <!-- <tris-checkbox
+                          :state="source.enabled"
+                          @state="onElementStateChanged"
+                        ></tris-checkbox> -->
                       </div>
                     </td>
-                    <td
-                      class="px-6 py-4 text-right whitespace-nowrap text-sm font-medium text-gray-900"
-                    >
+                    <td class="px-6 py-4 text-right whitespace-nowrap text-sm font-medium text-gray-900">
                       {{ source.id }}
                     </td>
-                    <td
-                      class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
-                    >
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                       {{ source.name }}
                     </td>
-                    <td
-                      class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
-                    >
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                       {{ source.template.name }}
                     </td>
-                    <td
-                      class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
-                    >
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                       {{ source.template.category.name }}
                     </td>
-                    <td
-                      class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
-                    >
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                       <a
                         :class="{
                           'font-bold text-green-700 cursor-pointer hover:text-green-500':
@@ -123,30 +120,18 @@
                         }}
                       </a>
                     </td>
-                    <td
-                      class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex gap-2"
-                    >
-                      <inertia-link
-                        :href="route('objects.sources.show', source.id)"
-                      >
-                        <detail-icon
-                          class="text-gray-500 font-medium text-sm w-5"
-                        ></detail-icon>
+                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium flex gap-2">
+                      <inertia-link :href="route('objects.sources.show', source.id)">
+                        <detail-icon class="text-gray-500 font-medium text-sm w-5"></detail-icon>
                       </inertia-link>
-                      <inertia-link
-                        :href="route('objects.sources.edit', source.id)"
-                      >
-                        <edit-icon
-                          class="text-gray-500 font-medium text-sm w-5"
-                        ></edit-icon>
+                      <inertia-link :href="route('objects.sources.edit', source.id)">
+                        <edit-icon class="text-gray-500 font-medium text-sm w-5"></edit-icon>
                       </inertia-link>
                       <button
                         class="focus:outline-none"
                         @click="onDelete(source.location)"
                       >
-                        <trash-icon
-                          class="text-red-500 font-medium text-sm w-5"
-                        ></trash-icon>
+                        <trash-icon class="text-red-500 font-medium text-sm w-5"></trash-icon>
                       </button>
                     </td>
                   </tr>
@@ -163,10 +148,14 @@
             </div>
           </div>
         </div>
+        <pre>{{ selected }}</pre>
       </div>
 
       <div class="w-full h-full md:w-1/2">
-        <leaflet-map :markers="markers" ref="map"></leaflet-map>
+        <leaflet-map
+          :markers="markers"
+          ref="map"
+        ></leaflet-map>
       </div>
     </div>
 
@@ -179,110 +168,129 @@
 </template>
 
 <script>
-import { watch, ref, onMounted } from "vue";
-import { Inertia } from "@inertiajs/inertia";
+  import { watch, ref, onMounted } from "vue";
+  import { Inertia } from "@inertiajs/inertia";
 
-import useUniqueLocations from "@/Composables/useUniqueLocations";
+  import useUniqueLocations from "@/Composables/useUniqueLocations";
 
-import AppLayout from "@/Layouts/AppLayout";
-import LeafletMap from "@/Components/LeafletMap";
-import TrisCheckbox from "@/Components/Forms/TrisCheckbox";
-import PrimaryLinkButton from "@/Components/PrimaryLinkButton";
-import TrashIcon from "@/Icons/TrashIcon.vue";
-import EditIcon from "@/Icons/EditIcon.vue";
-import DetailIcon from "@/Icons/DetailIcon.vue";
+  import AppLayout from "@/Layouts/AppLayout";
+  import LeafletMap from "@/Components/LeafletMap";
+  import TrisCheckbox from "@/Components/Forms/TrisCheckbox";
+  import PrimaryLinkButton from "@/Components/PrimaryLinkButton";
+  import TrashIcon from "@/Icons/TrashIcon.vue";
+  import EditIcon from "@/Icons/EditIcon.vue";
+  import DetailIcon from "@/Icons/DetailIcon.vue";
 
-export default {
-  components: {
-    AppLayout,
-    LeafletMap,
-    PrimaryLinkButton,
-    TrashIcon,
-    EditIcon,
-    DetailIcon,
-    TrisCheckbox,
-  },
-
-  props: {
-    sources: {
-      type: Array,
-      required: true,
+  export default {
+    components: {
+      AppLayout,
+      LeafletMap,
+      PrimaryLinkButton,
+      TrashIcon,
+      EditIcon,
+      DetailIcon,
+      TrisCheckbox
     },
-  },
 
-  setup(props) {
-    const map = ref(null);
-    const markers = ref([]);
-    const selected = ref([]);
-    const massAction = ref(true);
+    props: {
+      sources: {
+        type: Array,
+        required: true,
+      },
+    },
 
-    const uniqueSourceLocations = useUniqueLocations(props.sources);
+    setup(props) {
+      const map = ref(null);
+      const markers = ref([]);
 
-    // Populate the map with unique locations only.
-    for (const _source of uniqueSourceLocations.value) {
-      markers.value.push(_source.data);
-    }
+      const uniqueSourceLocations = useUniqueLocations(props.sources);
 
-    onMounted(() => {
-      selectAll();
-    });
-
-    function selectAll() {
-      // Select (checked) all the applicable sources
-      for (const _source of props.sources) {
-        if (!_source.location) continue;
-        selected.value.push(_source);
-      }
-    }
-
-    function deSelectAll() {
-      selected.value = [];
-    }
-
-    watch(massAction, (selected) => {
-      if (selected) {
-        selectAll();
-        return;
-      }
-      deSelectAll();
-    });
-
-    watch(selected, (selected, prevSelected) => {
-      // massAction.value = false;
-      if (!selected.length) {
-        markers.value = [];
-        return;
-      }
-
-      for (const _source of selected) {
-        const marker = uniqueSourceLocations.value.find(
-          (location) => location.id === _source.id
-        );
-        if (marker) continue;
+      // Populate the map with unique locations only.
+      for (const _source of uniqueSourceLocations.value) {
         markers.value.push(_source.data);
       }
-    });
 
-    function onDelete(source) {
-      // show modal here
-      Inertia.delete(route("objects.sources.destroy", source.id));
-    }
+      const massSelection = ref(true);
+      const massSelectionIndeterminated = ref(false);
+      const selected = ref([]);
 
-    function centerAtLocation(location) {
-      if (!markers.value.length) return;
+      let allSelectedLength;
 
-      const marker = markers.value.find((m) => m.id === location.geo_object.id);
-      map.value.centerAtLocation(marker);
-    }
+      onMounted(() => {
+        selectAll();
+      });
 
-    return {
-      map,
-      markers,
-      selected,
-      massAction,
-      onDelete,
-      centerAtLocation,
-    };
-  },
-};
+      function selectAll() {
+        // Select (checked) all the applicable sources
+        for (const _source of props.sources) {
+          if (!_source.location) continue;
+          selected.value.push(_source);
+        }
+        allSelectedLength = selected.value.length;
+      }
+
+      function deSelectAll() {
+        selected.value = [];
+      }
+
+      watch(selected, (current) => {
+        if (current.length === allSelectedLength) {
+          massSelection.value = true;
+          massSelectionIndeterminated.value = false;
+          return;
+        }
+
+        if (!current.length) {
+          massSelectionIndeterminated.value = false;
+          return;
+        }
+
+        massSelection.value = false;
+        massSelectionIndeterminated.value = true;
+      });
+
+      watch(massSelection, (current, previous) => {
+        if (previous && selected.value.length === allSelectedLength) {
+          deSelectAll();
+          return;
+        }
+
+        if (!previous && !selected.value.length) {
+          selectAll();
+          return;
+        }
+
+        if (current && selected.value.length && selected.value.length !== allSelectedLength) {
+          selectAll();
+          massSelectionIndeterminated.value = false;
+          return;
+        }
+      });
+
+      function onDelete(source) {
+        // show modal here
+        Inertia.delete(route("objects.sources.destroy", source.id));
+      }
+
+      function centerAtLocation(location) {
+        if (!markers.value.length) return;
+
+        const marker = markers.value.find((m) => m.id === location.geo_object.id);
+        map.value.centerAtLocation(marker);
+      }
+
+      return {
+
+        massSelectionIndeterminated,
+        massSelection,
+        selected,
+
+        map,
+        markers,
+
+        onDelete,
+        centerAtLocation,
+      };
+    },
+  };
 </script>
