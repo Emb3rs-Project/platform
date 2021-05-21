19f770d8c8a77084a02ccd4238abd0aa46b53702
George Mantellos <gmantellos@pdmfc.com>
2021-05-21 15:52:29 +0200

    :sparkles: Initial Factories

---
+++ database/factories/TemplatePropertyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TemplateProperty;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TemplatePropertyFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TemplateProperty::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
