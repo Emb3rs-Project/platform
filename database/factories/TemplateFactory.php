7bc2d6eb797f88beae31ef702854376aaef70234
dfernandes <davidsf@pantherify.com>
2021-05-24 12:59:59 +0200

    mini update

--- database/factories/TemplateFactory.php
+++ database/factories/TemplateFactory.php
@@ -22,7 +22,30 @@ class TemplateFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "name" => $this->faker->colorName . " Template",
+            "values" => []
         ];
+    }
+
+
+    public function isProcess()
+    {
+        return $this->state([
+            "name" => fn () => $this->faker->colorName . " Process [Seed]",
+        ]);
+    }
+
+    public function isSink()
+    {
+        return $this->state([
+            "name" => $this->faker->colorName . " Sink [Seed]"
+        ]);
+    }
+
+    public function isSource()
+    {
+        return $this->state([
+            "name" => $this->faker->colorName . " Source [Seed]"
+        ]);
     }
 }
--- database/seeders/ProcessSeeder.php
+++ database/seeders/ProcessSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Template;
 use Illuminate\Database\Seeder;
+use Log;
+
+// use Log;
 
 class ProcessSeeder extends Seeder
 {
@@ -14,5 +18,16 @@ class ProcessSeeder extends Seeder
      */
     public function run()
     {
+        $category = Category::whereName("Processes")->first();
+        if (!$category) $category = Category::create(["name" => "Processes", "type" => "process"]);
+
+
+        $processes = Template::factory()
+            ->count(5)
+            ->isProcess()
+            ->for($category)
+            ->create();
+
+        Log::debug("Created Processes");
     }
 }
