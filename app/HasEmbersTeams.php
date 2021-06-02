8cabc942e24cce5d8fbeee5289847a101dbab801
George Mantellos <gmantellos@pdmfc.com>
2021-06-02 08:44:49 +0200

    :recycle: :passport_control: Use a trait for Model functions in order to be DRY

---
+++ app/HasEmbersTeams.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App;
+
+use App\Models\TeamRole;
+use Illuminate\Support\Arr;
+use Laravel\Jetstream\Jetstream;
+
+trait HasEmbersTeams
+{
+    /**
+     * Get all of the teams the user belongs to.
+     *
+     * Note: This function is overriding the teams() function from
+     *       HasTeams trait, so it can be adapted to EMB3Rs use case.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function teams()
+    {
+        return $this->belongsToMany(
+            Jetstream::teamModel(),
+            Jetstream::membershipModel(),
+            'user_id',
+            'team_id'
+        )->withPivot('team_role_id')->withTimestamps()->as('membership');
+    }
+
+    /**
+     * Get the role that the user has on the team.
+     *
+     * Note: This function is overriding the teamRole() function from
+     *       HasTeams trait, so it can be adapted to EMB3Rs use case.
+     *
+     * @param  mixed  $team
+     * @return mixed
+     */
+    public function teamRole($team)
+    {
+        if ($this->ownsTeam($team)) {
+            return 'Owner';
+        }
+
+        if (! $this->belongsToTeam($team)) {
+            return;
+        }
+
+        return TeamRole::where('id', $team->users->where('id', $this->id)->first()->membership->team_role_id)
+            ->first();
+    }
+
+    /**
+     * Determine if the user has the given role on the given team.
+     *
+     * Note: This function is overriding the hasTeamRole() function from
+     *       HasTeams trait, so it can be adapted to EMB3Rs use case.
+     *
+     * @param  mixed  $team
+     * @param  string  $role
+     * @return bool
+     */
+    public function hasTeamRole($team, string $role)
+    {
+        if ($this->ownsTeam($team)) {
+            return true;
+        }
+
+        return $this->belongsToTeam($team) &&
+            TeamRole::where('id', $team->users->where('id', $this->id)->first()->membership->team_role_id)
+                ->first(['role']) === $role;
+    }
+
+    /**
+    * Get the user's permissions for the given team.
+    *
+    * Note: This function is overriding the teamPermissions() function from
+    *       HasTeams trait, so it can be adapted to EMB3Rs use case.
+    *
+    * @param  mixed  $team
+    * @return array
+    */
+    public function teamPermissions($team)
+    {
+        if ($this->ownsTeam($team)) {
+            return ['*'];
+        }
+
+        if (! $this->belongsToTeam($team)) {
+            return [];
+        }
+
+        $permissions = $this->teamRole($team)->permissions;
+
+        foreach ($permissions['permissions'] as &$permission) {
+            $permission = $permission['name'];
+        }
+        unset($permission);
+
+        return Arr::flatten($permissions);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
+use App\HasEmbersTeams;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Arr;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
-use Laravel\Jetstream\Jetstream;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -18,6 +17,12 @@ class User extends Authenticatable
     use HasFactory;
     use HasProfilePhoto;
     use HasTeams;
+    use HasEmbersTeams {
+        HasEmbersTeams::teams insteadof HasTeams;
+        HasEmbersTeams::teamRole insteadof HasTeams;
+        HasEmbersTeams::hasTeamRole insteadof HasTeams;
+        HasEmbersTeams::teamPermissions insteadof HasTeams;
+    }
     use Notifiable;
     use TwoFactorAuthenticatable;
 
@@ -61,94 +66,4 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
-
-    /**
-    * Get all of the teams the user belongs to.
-    *
-    * Note: This function is overriding the teams() function from
-    *       HasTeams trait, so it can be adapted to EMB3Rs use case.
-    *
-    * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-    */
-    public function teams()
-    {
-        return $this->belongsToMany(
-            Jetstream::teamModel(),
-            Jetstream::membershipModel(),
-            'user_id',
-            'team_id'
-        )->withPivot('team_role_id')->withTimestamps()->as('membership');
-    }
-
-    /**
-    * Get the role that the user has on the team.
-    *
-    * Note: This function is overriding the teamRole() function from
-    *       HasTeams trait, so it can be adapted to EMB3Rs use case.
-    *
-    * @param  mixed  $team
-    * @return mixed
-    */
-    public function teamRole($team)
-    {
-        if ($this->ownsTeam($team)) {
-            return 'Owner';
-        }
-
-        if (! $this->belongsToTeam($team)) {
-            return;
-        }
-
-        return TeamRole::where('id', $team->users->where('id', $this->id)->first()->membership->team_role_id)->first();
-    }
-
-    /**
-     * Determine if the user has the given role on the given team.
-     *
-     * Note: This function is overriding the hasTeamRole() function from
-     *       HasTeams trait, so it can be adapted to EMB3Rs use case.
-     *
-     * @param  mixed  $team
-     * @param  string  $role
-     * @return bool
-     */
-    public function hasTeamRole($team, string $role)
-    {
-        if ($this->ownsTeam($team)) {
-            return true;
-        }
-
-        return $this->belongsToTeam($team) &&
-               TeamRole::where('id', $team->users->where('id', $this->id)->first()->membership->team_role_id)
-                   ->first(['role']) === $role;
-    }
-
-    /**
-    * Get the user's permissions for the given team.
-    *
-    * Note: This function is overriding the teamPermissions() function from
-    *       HasTeams trait, so it can be adapted to EMB3Rs use case.
-    *
-    * @param  mixed  $team
-    * @return array
-    */
-    public function teamPermissions($team)
-    {
-        if ($this->ownsTeam($team)) {
-            return ['*'];
-        }
-
-        if (! $this->belongsToTeam($team)) {
-            return [];
-        }
-
-        $permissions = $this->teamRole($team)->permissions;
-
-        foreach ($permissions['permissions'] as &$permission) {
-            $permission = $permission['name'];
-        }
-        unset($permission);
-
-        return Arr::flatten($permissions);
-    }
 }
