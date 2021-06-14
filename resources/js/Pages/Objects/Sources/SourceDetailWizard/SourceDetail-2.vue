c024bf97e7fbe9b0e88502826fd658bcf86ecbbd
dfernandes <davidsf@pantherify.com>
2021-06-14 11:35:54 +0200

    upgrades

--- resources/js/Pages/Objects/Sources/SourceDetailWizard/SourceDetail-2.vue
+++ resources/js/Pages/Objects/Sources/SourceDetailWizard/SourceDetail-2.vue
@@ -1,8 +1,100 @@
 <template>
-
-<pre>{{instance.values}}</pre>
-
-
+  <h1 class="my-4 ml-4">Equipment</h1>
+  <Disclosure
+    v-slot="{ open }"
+    v-for="equip of instance.values.equipments"
+    :key="equip.key"
+  >
+    <DisclosureButton
+      class="
+        flex
+        justify-between
+        w-full
+        px-4
+        py-2
+        text-sm
+        font-medium
+        text-left
+      "
+    >
+      <span>{{ equip.value }}</span>
+      <ChevronUpIcon
+        :class="open ? 'transform rotate-180' : ''"
+        class="w-5 h-5"
+      />
+    </DisclosureButton>
+    <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+      <h1 class="px-4 py-3 font-bold">Properties :</h1>
+      <div
+        v-for="(datum, name) of equip.data"
+        :key="datum"
+        class="
+          space-y-1
+          px-4
+          sm:space-y-0
+          sm:grid sm:grid-cols-3
+          sm:gap-4
+          sm:px-6
+          sm:py-2
+          border-b-[1px]
+        "
+      >
+        <div>
+          <label class="block text-sm font-medium text-gray-500 sm:pt-1">
+            {{ getName(equip, name) }}
+          </label>
+        </div>
+        <div class="sm:col-span-2">
+          <div class="block text-sm font-medium text-gray-900 sm:pt-1">
+            {{ datum.value ? datum.value : datum }}
+          </div>
+        </div>
+      </div>
+      <h1 class="px-4 py-3 font-bold">Calculated :</h1>
+      <div
+        class="
+          space-y-1
+          px-4
+          sm:space-y-0
+          sm:grid sm:grid-cols-3
+          sm:gap-4
+          sm:px-6
+          sm:py-2
+          border-b-[1px]
+        "
+      >
+        <div>
+          <label class="block text-sm font-medium text-gray-500 sm:pt-1">
+            XPTO
+          </label>
+        </div>
+        <div class="sm:col-span-2">
+          <div class="block text-sm font-medium text-gray-900 sm:pt-1">10</div>
+        </div>
+      </div>
+      <div
+        class="
+          space-y-1
+          px-4
+          sm:space-y-0
+          sm:grid sm:grid-cols-3
+          sm:gap-4
+          sm:px-6
+          sm:py-2
+          border-b-[1px]
+        "
+      >
+        <div>
+          <label class="block text-sm font-medium text-gray-500 sm:pt-1">
+            XPTO
+          </label>
+        </div>
+        <div class="sm:col-span-2">
+          <div class="block text-sm font-medium text-gray-900 sm:pt-1">10</div>
+        </div>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
 <script>
@@ -15,6 +107,8 @@ import SelectMenu from "@/Components/NewLayout/Forms/SelectMenu.vue";
 import TextInput from "@/Components/NewLayout/Forms/TextInput.vue";
 import PrimaryButton from "@/Components/NewLayout/PrimaryButton.vue";
 import SecondaryOutlinedButton from "@/Components/NewLayout/SecondaryOutlinedButton.vue";
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { ChevronUpIcon } from "@heroicons/vue/solid";
 
 export default {
   components: {
@@ -24,6 +118,10 @@ export default {
     TextInput,
     PrimaryButton,
     SecondaryOutlinedButton,
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    ChevronUpIcon,
   },
 
   props: {
@@ -33,8 +131,19 @@ export default {
     },
   },
 
-  setup() {
-    return {};
+  setup(props) {
+    console.log(props.instance);
+
+    const getName = (equip, name) => {
+      const newName = equip.props.find(
+        (p) => p.property.symbolic_name === name
+      )?.property;
+      return newName?.name;
+    };
+
+    return {
+      getName,
+    };
   },
 };
 </script>
