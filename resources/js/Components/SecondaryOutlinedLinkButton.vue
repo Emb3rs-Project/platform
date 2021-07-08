9e57fb89623ad6af5b17370a74b798a7fcf0928d
George Mantellos <gmantellos@pdmfc.com>
2021-07-08 12:31:39 +0200

    UI

--- resources/js/Components/SecondaryOutlinedLinkButton.vue
+++ resources/js/Components/SecondaryOutlinedLinkButton.vue
@@ -6,7 +6,7 @@
       disabled
         ? 'cursor-default disabled:opacity-50 hover:bg-white'
         : 'hover:bg-gray-50',
-      'inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium uppercase tracking-widest rounded-md text-gray-700 bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+      'inline-flex justify-center items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium uppercase tracking-widest rounded-md text-gray-700 bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
     ]"
     :disabled="disabled"
   >
