7fd03ada9e85b8de6a72da082e04730869c72844
George Mantellos <gmantellos@pdmfc.com>
2021-07-16 09:00:44 +0200

    Use recomended way of registering inertiajs on the client-side

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,23 @@
 import 'es6-promise/auto';
 import 'leaflet/dist/leaflet.css';
 
-import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress';
-import { createApp, h } from 'vue';
 
 import { store } from './Stores';
 
 require('./bootstrap');
 
-// Import modules...
-const el = document.getElementById('app');
-
-createApp({
-    render: () =>
-        h(InertiaApp, {
-            initialPage: JSON.parse(el.dataset.page),
-            resolveComponent: (name) => require(`./Pages/${name}`).default,
-        }),
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`).default,
+  setup({ el, app, props, plugin }) {
+    createApp({ render: () => h(app, props) })
+      .mixin({ methods: { route } })
+      .use(plugin)
+      .use(store)
+      .mount(el)
+  },
 })
-    .mixin({ methods: { route } })
-    .use(InertiaPlugin)
-    .use(store)
-    .mount(el);
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#F59E0B' });
