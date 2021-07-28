edfbc29a16c4934c355dfa2a2ede715b582f5027
George Mantellos <gmantellos@pdmfc.com>
2021-07-28 10:56:19 +0200

    UI changes on buttons

--- resources/js/Components/SecondaryOutlinedButton.vue
+++ resources/js/Components/SecondaryOutlinedButton.vue
@@ -2,8 +2,8 @@
   <button
     :type="type"
     :class="[
-      disabled ? 'cursor-default disabled:opacity-50' : '',
-      'inline-flex justify-center items-center px-3 py-2 leading-4 border border-gray-300 shadow-sm text-sm font-medium uppercase tracking-widest rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+      disabled ? 'cursor-default disabled:opacity-50' : 'hover:bg-gray-50',
+      'inline-flex justify-center items-center px-3 py-2 leading-4 border border-gray-300 shadow-sm text-sm font-medium uppercase tracking-widest rounded-md text-gray-700 bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
     ]"
     :disabled="disabled"
   >
