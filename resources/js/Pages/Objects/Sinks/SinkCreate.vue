55389ff176acad6ca1be4f4b6554c8f3da7347a7
George Mantellos <gmantellos@pdmfc.com>
2021-11-18 13:34:11 +0100

    temp solution to see the errors of sink creation untill frontend is ready

--- resources/js/Pages/Objects/Sinks/SinkCreate.vue
+++ resources/js/Pages/Objects/Sinks/SinkCreate.vue
@@ -61,6 +61,7 @@
           />
         </div>
         <div v-if="form.hasErrors">
+          <pre>{{form.errors}}</pre>
           <div
             v-for="(error, key) in form.errors"
             :key="key"
@@ -211,7 +212,7 @@ export default {
     watch(
       selectedTemplate,
       (template) => {
-        form.sink.data = {};
+        // form.sink.data = {};
 
         templateInfo.value = templates.value.find(
           (t) => t.key === template.key
