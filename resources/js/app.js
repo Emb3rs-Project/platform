bd8c51d55c905b9bbc0611ea3d7580c9243ad950
George Mantellos <gmantellos@pdmfc.com>
2022-01-10 18:16:21 +0100

    Update the platform php and npm deps

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,8 +15,8 @@ require('./bootstrap');
 createInertiaApp({
   title: title => title ? `${title} | EMB3R's` : `EMB3R's`,
   resolve: name => require(`./Pages/${name}`).default,
-  setup({ el, app, props, plugin }) {
-    createApp({ render: () => h(app, props) })
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
       .mixin({ methods: { route: route } })
       .use(plugin)
       .use(store)
