3fac3be6aa6e8d62f310cafe623e9d790da35a2a
George Mantellos <gmantellos@pdmfc.com>
2022-01-10 18:15:37 +0100

    New helpers

--- resources/js/Utils/helpers.js
+++ resources/js/Utils/helpers.js
@@ -5,3 +5,77 @@ export const sortProperties = (properties) => {
     )
     : properties;
 };
+
+export const validateProperies = (parent, properties, index = null) => {
+  const errors = {};
+
+  for (const property of properties) {
+    const propertyErrors = [];
+
+    const inputType = property.property.inputType;
+    const dataType = property.property.dataType;
+    const symbolicName = property.property.symbolic_name;
+    const value = parent.data[property.property.symbolic_name];
+    const propertyName = property.property.name.toLowerCase();
+
+    let propertyCopy = value;
+
+    if (inputType === "select") {
+      // if the property has a value, get it and re-assign the property as a string
+      if (Object.keys(value).length) {
+        propertyCopy = value.key;
+      } else {
+        propertyCopy = "";
+      }
+    }
+
+    if (property.required) {
+      if (!propertyCopy)
+        propertyErrors.push(`The ${propertyName} field is required.`);
+    }
+
+    switch (dataType.toLowerCase()) {
+      case "text":
+        if (typeof propertyCopy !== "string")
+          propertyErrors.push(
+            `The ${propertyName} field must be a string.`
+          );
+        break;
+      case "string":
+        if (typeof propertyCopy !== "string")
+          propertyErrors.push(
+            `The ${propertyName} field must be a string.`
+          );
+        break;
+      case "number":
+        if (isNaN(propertyCopy))
+          propertyErrors.push(
+            `The ${propertyName} field must be numeric.`
+          );
+        break;
+      case "float":
+        if (isNaN(propertyCopy))
+          propertyErrors.push(
+            `The ${propertyName} field must be numeric.`
+          );
+        break;
+      case "datetime":
+        // TODO: validate the datetime
+        break;
+
+      default:
+        break;
+    }
+
+    if (!propertyErrors.length) continue;
+
+    // let key = `${type}.data.${symbolicName}`;
+    let key = `${symbolicName}`;
+
+    if (index) key = `${index}.${key}`;
+
+    errors[key] = propertyErrors;
+  }
+
+  return errors;
+};
