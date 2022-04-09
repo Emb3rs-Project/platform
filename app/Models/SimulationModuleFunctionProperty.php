a5c998228050d7d190aa507811871cb4f76731bf
David Fernandes <davidsf@pantherify.com>
2022-04-09 10:11:06 +0000

    update

---
+++ app/Models/SimulationModuleFunctionProperty.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SimulationModuleFunctionProperty extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    public function property(): BelongsTo
+    {
+        return $this->belongsTo(Property::class, "property_id");
+    }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class, "unit_id");
+    }
+
+    public function simulation_functions(): BelongsTo
+    {
+        return $this->belongsTo(SimulationModuleFunction::class, 'simulation_module_id');
+    }
+}
---
+++ database/migrations/2022_04_09_095513_create_simulation_module_function_properties_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('simulation_module_function_properties', function (Blueprint $table) {
+            $table->id();
+
+            $table->boolean('required')->default(false);
+            $table->boolean('advanced')->default(false);
+            $table->integer('order')->unsigned();
+            $table->string('default_value', 100)->nullable();
+
+            $table->foreignId("simulation_module_function_id");
+            $table->foreignId("property_id");
+            $table->foreignId("unit_id");
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('simulation_module_function_properties');
+    }
+};
