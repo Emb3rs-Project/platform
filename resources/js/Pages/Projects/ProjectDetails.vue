ba7acb69b0b09d97e61b882b2afcf6cc8914cc17
David Fernandes <davidsf@pantherify.com>
2022-05-19 12:31:55 +0000

    update views

--- resources/js/Pages/Projects/ProjectDetails.vue
+++ resources/js/Pages/Projects/ProjectDetails.vue
@@ -193,7 +193,7 @@
                                                 <td
                                                     class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
                                                 >
-                                                    {{ simulation.extra.steps }}
+                                                    n
                                                     Steps
                                                 </td>
                                                 <td
