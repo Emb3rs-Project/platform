c4e2d63df7d58f46008e3b3f112b88eff7102566
David Fernandes <davidsf@pantherify.com>
2022-05-23 10:20:45 +0000

    added building

--- app/Actions/Embers/Integration/CharacterizeInstance.php
+++ app/Actions/Embers/Integration/CharacterizeInstance.php
@@ -6,25 +6,45 @@ use App\Contracts\Embers\Integration\CharacterizesInstances;
 use App\Models\Instance;
 use Cf\CFModuleClient;
 use Cf\CharacterizationInput;
+use Cf\CharacterizationSinkOutput;
 use Illuminate\Support\Facades\Redis;
 
 class CharacterizeInstance implements CharacterizesInstances
 {
     public function characterize(Instance $instance): void
     {
-        // Prepare data for characterization, if trigger exists
-        $template = $instance->template;
-        $type = $template->category->type;
-
         $client = new CFModuleClient(
-            'vali.pantherify.dev:50051', [
+            'vali.pantherify.dev:50051',
+            [
                 'credentials' => \Grpc\ChannelCredentials::createInsecure(),
             ]
         );
 
+        switch ($instance->template_id) {
+            case 14:
+            case 15:
+                $this->simple_user($instance, $client);
+                break;
+
+            case 2:
+                $this->building($instance, $client);
+                break;
+
+            default:
+                dump("NOT DEFINED!");
+                break;
+        }
+    }
+
+    private function simple_user(Instance $instance, CFModuleClient $client)
+    {
+        $template = $instance->template;
+        $type = $template->category->type;
+
         $data = $instance->values;
 
-        if($template->id == 15)
+        // TODO: Normalize Values Structure
+        if ($template->id == 15)
             $data = $instance->values["properties"];
 
         $request = new CharacterizationInput();
@@ -33,17 +53,40 @@ class CharacterizeInstance implements CharacterizesInstances
             "streams" => [$data]
         ]));
 
-         /** @var CharacterizationSourceOutput $feature */
-         list($feature, $status) = $client->char_simple($request)->wait();
+        /** @var CharacterizationSourceOutput $feature */
+        list($feature, $status) = $client->char_simple($request)->wait();
 
-         $characterization = [];
 
-         if($feature) {
-             $values = $instance->values;
-             $characterization["streams"] = json_decode($feature->getStreams());
-             $values['characterization'] = $characterization;
-             $instance->values = $values;
-             $instance->save();
-         }
+        if ($feature) {
+            $characterization = [];
+            $values = $instance->values;
+            $characterization["streams"] = json_decode($feature->getStreams());
+            $values['characterization'] = $characterization;
+            $instance->values = $values;
+            $instance->save();
+        }
+    }
+
+    private function building(Instance $instance, CFModuleClient $client)
+    {
+        $data = $instance->values;
+        $location = $instance->location->data;
+        $data["location"] = $location['center'];
+
+        $request = new CharacterizationInput();
+        $request->setPlatform(json_encode($data));
+
+        /** @var CharacterizationSinkOutput $feature */
+        list($feature, $status) = $client->char_building($request)->wait();
+
+        if($feature) {
+            $characterization = [];
+            $values = $instance->values;
+            $characterization["streams"] = [json_decode($feature->getColdStream()), json_decode($feature->getHotStream())];
+            $values['characterization'] = $characterization;
+            $instance->values = $values;
+            $instance->save();
+        }
+
     }
 }
