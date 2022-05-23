edabc9af1d251d986c896aabdc06298f0e5068d1
David Fernandes <davidsf@pantherify.com>
2022-05-23 09:18:38 +0000

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Instance;
+use App\Models\Property;
+use App\Models\Template;
+use App\Models\TemplateProperty;
+use App\Models\Unit;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +20,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        Instance::truncate();
+       Template::truncate();
+       TemplateProperty::truncate();
+       Property::truncate();
+       Unit::truncate();
+       Category::truncate();
     }
 }
