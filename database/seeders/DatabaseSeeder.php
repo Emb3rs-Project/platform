ea9d89c716ba423891dde6408e99e80995d057ab
David Fernandes <davidsf@pantherify.com>
2022-05-23 09:14:39 +0000

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Instance;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            CategorySeeder::class,
-            EquipmentSeeder::class,
-            PropertySeeder::class,
-            UnitSeeder::class,
-        ]);
+       $instances = Instance::all();
+       $instances->truncate();
     }
 }
