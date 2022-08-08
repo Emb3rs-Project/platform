f4df056606df2aecbfd29ee6daffc9f00090fab1
plima <pedro.lima@pdmfc.com>
2022-08-08 23:55:34 +0100

    adjusment on the grpc integration

--- config/grpc.php
+++ config/grpc.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    "GRPC_MANAGER_HOST" =>  env('GRPC_MANAGER_HOST', 'localhost'),
-    "GRPC_MANAGER_PORT" => env('GRPC_MANAGER_PORT', '50041'),
-    "GRPC_CF_HOST" => env('GRPC_CF_HOST', 'localhost'),
-    "GRPC_CF_PORT" => env('GRPC_CF_PORT', '50051'),
+    "grpc_manager_host" =>  env('GRPC_MANAGER_HOST', 'ms_manager'),
+    "grpc_manager_port" => env('GRPC_MANAGER_PORT', '50041'),
+    "grpc_cf_host" => env('GRPC_CF_HOST', 'm_cf'),
+    "grpc_cf_port" => env('GRPC_CF_PORT', '50051'),
 ];
