9e3b0f10d749ba71fe8f97bba5be5aa41f409bf9
Lucas Lima <lucas.lima@pdmfc.com>
2022-09-15 00:02:07 +0100

    wip map

--- routes/web.php
+++ routes/web.php
@@ -143,6 +143,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::delete('/sessions/{session}', [ProjectSimulationSessionController::class, 'destroy'])->name("session.delete");
 
     Route::get('/sessions/{session}/report/{report}', ProjectSimulationSessionReportController::class)->name('session.report.show');
+    Route::get('/sessions/{session}/map', [ProjectSimulationSessionReportController::class, 'getMapData'])->name('session.map.show');
     Route::get('/sessions/{session}/final-report', [ProjectSimulationSessionReportController::class, 'getFinalReport'])->name('session.final-report.show');
 
     // Projects
