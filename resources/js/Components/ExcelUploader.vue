e221997d78d8f89d714398463d025eec30c0839c
Lucas Lima <lucas.lima@pdmfc.com>
2022-11-23 01:57:48 +0000

    field excel extractor

---
+++ resources/js/Components/ExcelUploader.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <input type="file" @change="onChange"/>
+        <xlsx-read :file="file">
+            <xlsx-json @parsed="setCollection">
+            </xlsx-json>
+        </xlsx-read>
+    </div>
+</template>
+
+
+<script>
+import {XlsxRead, XlsxJson} from "vue3-xlsx";
+
+export default {
+    components: {
+        XlsxRead,
+        XlsxJson
+    },
+    data () {
+        return {
+            file: null,
+            data: []
+        };
+    },
+    methods: {
+        onChange (event) {
+            this.file = event.target.files ? event.target.files[0] : null;
+        },
+        setCollection (collection) {
+            this.data = collection
+            this.$emit('input', collection)
+        }
+    }
+};
+</script>
