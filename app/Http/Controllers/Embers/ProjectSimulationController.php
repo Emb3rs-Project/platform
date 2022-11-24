909b4424ca9ddd52c7d8807bab82f3f179809606
plima <pedro.lima@pdmfc.com>
2022-11-24 11:15:38 +0000

    feat: solver selector

--- app/Http/Controllers/Embers/ProjectSimulationController.php
+++ app/Http/Controllers/Embers/ProjectSimulationController.php
@@ -97,9 +97,15 @@ class ProjectSimulationController extends Controller
             $extras['sources'] = [];
         }
 
-        if (is_array($extras['solver'])){
-            $extras['solver'] = $extras['solver']['id'];
+        $solvers = ['solver_teo','solver_gis','solver_market'];
+        foreach ($solvers as $solver) {
+            if (is_array($extras[$solver])){
+                $extras[$solver] = $extras[$solver]['id'];
+            }
+
         }
+
+
 
        $simulation = $project->simulations()->create([
             "status" => "NEW",
